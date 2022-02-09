78fd1ceee19e9d77dfe8a47171bcb9e884320106
Péter Csordás <csordaspeter469@gmail.com>
2022-02-09 10:26:19 +0100

    Add project files.

---
+++ IskolaGUI/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace IskolaGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTörlés = new System.Windows.Forms.Button();
+            this.btnMentés = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnTörlés
+            // 
+            this.btnTörlés.Location = new System.Drawing.Point(12, 421);
+            this.btnTörlés.Name = "btnTörlés";
+            this.btnTörlés.Size = new System.Drawing.Size(115, 23);
+            this.btnTörlés.TabIndex = 0;
+            this.btnTörlés.Text = "Törlés";
+            this.btnTörlés.UseVisualStyleBackColor = true;
+            this.btnTörlés.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMentés
+            // 
+            this.btnMentés.Location = new System.Drawing.Point(133, 421);
+            this.btnMentés.Name = "btnMentés";
+            this.btnMentés.Size = new System.Drawing.Size(115, 23);
+            this.btnMentés.TabIndex = 1;
+            this.btnMentés.Text = "Állomány mentése";
+            this.btnMentés.UseVisualStyleBackColor = true;
+            this.btnMentés.Click += new System.EventHandler(this.btnMentés_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 394);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 456);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnMentés);
+            this.Controls.Add(this.btnTörlés);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTörlés;
+        private System.Windows.Forms.Button btnMentés;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
